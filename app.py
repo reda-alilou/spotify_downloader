379d0d251ec08f49556784e23c57a74898ac12d9
Reda <redaalilou.al@gmail.com>
2024-09-29 14:51:11 +0100

    Updated app to bind to Heroku's port

--- app.py
+++ app.py
@@ -99,5 +99,6 @@ def download_tracks(playlist_id):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    # Get the PORT from the environment variables or use the default port 5000
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
